42577731976b3365e8d3f975015f837828bce1fc
Ekaterina Litvinova <e.litvinova@icloud.com>
2020-08-13 23:27:58 +0300

    Added task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const main = document.getElementById('modal_main');
+const success = document.getElementById('modal_success');
+
+main.classList.add('modal_active');
+
+const mainClose = main.querySelector('.modal__close');
+
+mainClose.onclick = function() {
+    main.classList.remove('modal_active');
+}
+
+const showSuccess = main.querySelector('.show-success');
+
+showSuccess.onclick = function() {
+    main.classList.remove('modal_active');
+    success.classList.add('modal_active');
+};
+
+function close() {
+    success.onclick = function() {
+        success.classList.remove('modal_active');
+    }
+}
+
+close();
